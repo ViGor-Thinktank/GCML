ad6387f5f55f7d00bcab32875e9f2427fcaaee24
Hagen Eckert <hagen.eckert@hotmail.com>
2012-05-25 16:43:29 +0200

    Änderung CampaignStateTest

--- GenericCampaignMasterLibTests/Tests/CampaignStateTest.cs
+++ GenericCampaignMasterLibTests/Tests/CampaignStateTest.cs
@@ -15,16 +15,15 @@ namespace GenericCampaignMasterLibTests.Tests
         {
             CampaignEngine engine_origin = new CampaignEngine();
             engine_origin.CField = new Field();
-            engine_origin.CPlayers = new List<Player>() { new Player(123) };
-
             engine_origin.CField.Id = 123;
+            engine_origin.CPlayers = new List<Player>() { new Player(123), new Player(456) };
 
             CampaignState state = new CampaignState();
             state.Save(engine_origin);
 
             CampaignEngine engine1 = state.Restore();
-            Assert.AreEqual(engine_origin.CField, engine1.CField, "CField aus dem wiederhergestellten State sinid nicht identisch.");
-            Assert.AreEqual(engine_origin.CPlayers, engine1.CPlayers, "CPlayers aus dem wiederhergestellten State sinid nicht identisch.");
+            Assert.AreEqual(engine_origin.CField, engine1.CField, "CField aus dem wiederhergestellten State ist nicht identisch.");
+            Assert.AreEqual(engine_origin.CPlayers, engine1.CPlayers, "CPlayers aus dem wiederhergestellten State ist nicht identisch.");
         }
         
     }       
