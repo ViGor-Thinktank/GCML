5c3232bff79124e0a1c4fcd8c40dc7b472274c08
Hagen Eckert <hagen.eckert@hotmail.com>
2012-05-25 10:08:25 +0200

    Einfache Implementierung CampaignState

--- GenericCampaignMasterLib/Code/CampaignState.cs
+++ GenericCampaignMasterLib/Code/CampaignState.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace GenericCampaignMasterLib
 {
     [Serializable()]
     public class CampaignState
     {
-
+        MemoryStream stream = new MemoryStream();
+        BinaryFormatter formatter = new BinaryFormatter();
+        
+        public CampaignState Save(CampaignEngine engine)
+        {
+            formatter.Serialize(stream, engine);
+            return this;
+        }
+        
+        public CampaignEngine Restore(CampaignState state)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            CampaignEngine engine = (CampaignEngine)formatter.Deserialize(stream);
+            return engine;
+        }
 
     }
 }
--- GenericCampaignMasterLib/Code/Unit.cs
+++ GenericCampaignMasterLib/Code/Unit.cs
@@ -5,10 +5,9 @@ using System.Text;
 
 namespace GenericCampaignMasterLib
 {
-    [Serializable()]
     public interface IUnit
     {
-        public int Id { get; set; }             // Dummy
-        public string Bezeichnung { get; set; } // Dummy
+         int Id { get; set; }             // Dummy
+         string Bezeichnung { get; set; } // Dummy
     }
 }
