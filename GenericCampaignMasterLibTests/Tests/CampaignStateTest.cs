79a16d7988f88a0c3f3eec63f3e0f0841689218b
Hagen Eckert <hagen.eckert@hotmail.com>
2012-05-25 16:25:23 +0200

    Projekt für Unit-Tests hinzugefügt

---
+++ GenericCampaignMasterLibTests/Tests/CampaignStateTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GenericCampaignMasterLib;
+using NUnit.Framework;
+
+namespace GenericCampaignMasterLibTests.Tests
+{
+    [TestFixture]
+    public class CampaignStateTest
+    {
+        [Test]
+        public void saveAndRestoreCampaignState()
+        {
+            CampaignEngine engine_origin = new CampaignEngine();
+            engine_origin.CField = new Field();
+            engine_origin.CPlayers = new List<Player>() { new Player(123) };
+
+            engine_origin.CField.Id = 123;
+
+            CampaignState state = new CampaignState();
+            state.Save(engine_origin);
+
+            CampaignEngine engine1 = state.Restore();
+            Assert.AreEqual(engine_origin.CField, engine1.CField, "CField aus dem wiederhergestellten State sinid nicht identisch.");
+            Assert.AreEqual(engine_origin.CPlayers, engine1.CPlayers, "CPlayers aus dem wiederhergestellten State sinid nicht identisch.");
+        }
+        
+    }       
+
+
+}
