e52a568c7d8c96254fc93269a929c00e6a6c33c7
Hagen <hgen@gmx.net>
2012-06-12 21:50:21 +0200

    Deserialisierung Units

--- GenericCampaignMasterLib/Code/CampaignState.cs
+++ GenericCampaignMasterLib/Code/CampaignState.cs
@@ -15,6 +15,7 @@ namespace GenericCampaignMasterLib
         public string playerId;
         public string sektorId;
         public string unitId;
+		public string unitType;
     }
 
     public class CampaignState : Dictionary<string, string>
@@ -24,19 +25,28 @@ namespace GenericCampaignMasterLib
 
         public List<Sektor> getListSektors()
         {
-            return (List<Sektor>) m_serializer.Deserialize(this["sektors"], typeof(List<Sektor>));
+            //return (List<Sektor>) m_serializer.Deserialize(this["sektors"], typeof(List<Sektor>));
+			return (List<Sektor>) m_serializer.Deserialize<List<Sektor>>(this["sektors"]);
         }
 
         public List<Player> getListPlayers()
         {
 
-            string p = this["players"];
-            return (List<Player>)m_serializer.Deserialize(this["players"], typeof(List<Player>));
+            //return (List<Player>)m_serializer.Deserialize(this["players"], typeof(List<Player>));
+			return (List<Player>)m_serializer.Deserialize<List<Player>>(this["players"]);
         }
+		
+		public List<UnitInfo> getListUnitInfo()
+		{	
+			string strUnitInfo = this["unitinfo"];
+			//List<UnitInfo> lstUnitInfo = (List<UnitInfo>)m_serializer.Deserialize(strUnitInfo, typeof(List<UnitInfo>));
+			List<UnitInfo> lstUnitInfo = (List<UnitInfo>)m_serializer.Deserialize<List<UnitInfo>>(strUnitInfo);
+			return lstUnitInfo;
+		}
 
         public List<int> getListDimensions()
         {
-            return (List<int>)m_serializer.Deserialize(this["fielddimension"], typeof(List<int>));
+            return (List<int>)m_serializer.Deserialize<List<int>>(this["fielddimension"]);
         }
 
         public string getFieldtype()
@@ -51,6 +61,10 @@ namespace GenericCampaignMasterLib
             this["sektors"] = m_serializer.Serialize (engine.FieldField.ListSektors.Values);
             this["fielddimension"] = m_serializer.Serialize(engine.FieldField.ListDimensions);
             this["fieldtype"] = engine.FieldField.GetType().ToString();
+			
+			List<UnitInfo> lstUnitInfo = engine.getUnitInfo();
+			this["unitinfo"] = m_serializer.Serialize(lstUnitInfo);
+			
             return this;
         }
         
@@ -58,15 +72,26 @@ namespace GenericCampaignMasterLib
         {
             List<Player> lstPlayers = getListPlayers();
             List<Sektor> lstSektors = getListSektors();
-
+		
             List<int> lstDim = getListDimensions();
 
             Type fieldType = Type.GetType(getFieldtype());
-            Field f = (Field) Activator.CreateInstance(fieldType, new object[]{ lstDim });
-
+            Field f = (Field) Activator.CreateInstance(fieldType, new object[]{ lstDim });	
+			
             CampaignEngine engine = new CampaignEngine((Field)f);
             engine.setPlayerList(lstPlayers);
 
+			foreach(UnitInfo uInfo in getListUnitInfo())
+			{
+				Type unitType = Type.GetType(uInfo.unitType);
+				UnitTypeBase newUnitType = (UnitTypeBase) Activator.CreateInstance(unitType);
+				
+				BaseUnit unit = new BaseUnit(Int32.Parse(uInfo.unitId), newUnitType);
+	
+				// TODO Koordinaten
+				engine.addUnit(Int32.Parse(uInfo.playerId), unit, f.nullSektorKoord);
+			}
+			
             return engine;
         }
 
