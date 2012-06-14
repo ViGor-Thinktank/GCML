e41de7f187d822d53bb06e81e0911d5a3be1e9a0
Hagen Eckert <hagen.eckert@hotmail.com>
2012-06-14 15:40:24 +0200

    CampaignDatabase hinzugefügt

---
+++ GenericCampaignMasterLib/Code/Db/CampaignDatabaseRaptorDb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCampaignMasterLib
+{
+    class CampaignDatabaseRaptorDb : ICampaignDatabase
+    {
+        #region ICampaignDatabase Member
+
+        public void initDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void saveGameState(CampaignState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CampaignState getLastGameState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CampaignState getCampaignStateByKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CampaignState getCampaignStateByDate(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CampaignState> getAllCampaignStates()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ GenericCampaignMasterLib/Code/Db/ICampaignDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCampaignMasterLib
+{
+    interface ICampaignDatabase
+    {
+        public void initDatabase();
+        public void saveGameState(CampaignState state);
+        public CampaignState getLastGameState();
+        public CampaignState getCampaignStateByKey(string key);
+        public CampaignState getCampaignStateByDate(DateTime time);
+        public List<CampaignState> getAllCampaignStates();
+    }
+}
