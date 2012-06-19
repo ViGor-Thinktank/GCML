06f4535ce9502baab6a40750505dc5ad0610dc1c
Hagen Eckert <hagen.eckert@hotmail.com>
2012-06-19 11:33:28 +0200

    Testklasse Testsetup entfernt.
    Setup kann von CampaignBuilder übernommen werden.

--- GenericCampaignMasterLib/Code/Builder/CampaignBuilderTicTacTod.cs
+++ GenericCampaignMasterLib/Code/Builder/CampaignBuilderTicTacTod.cs
@@ -22,15 +22,32 @@ namespace GenericCampaignMasterLib
         {
 			CampaignDatabaseRaptorDb database = new CampaignDatabaseRaptorDb("");
 			string campaignkey = database.initDatabase();
-			
-			Field_Schachbrett field = new Field_Schachbrett(new List<int>(){ 3, 3});
+
+
+			//Field_Schachbrett field = new Field_Schachbrett(new List<int>(){ 3, 3});
+            Field_Schlauch field = new Field_Schlauch(new List<int> { 3 });
 			CampaignEngine engine = new CampaignEngine(field);
-			
+            engine.FieldField.Id = 123;
+
+            Player player1 = new Player(1);
+            Player player2 = new Player(2);
+
+            DummyUnit unit1 = new DummyUnit(666);
+            DummyUnit unit2 = new DummyUnit(667);
+
+            player1.ListUnits.Add(unit1);
+            player2.ListUnits.Add(unit2);
+
+            engine.addPlayer(player1);
+            engine.addPlayer(player2);
+
+            //sektor1.addUnit(unit1);
+            //sektor2.addUnit(unit2);
+
 			CampaignController controller = new CampaignController();
 			controller.CampaignDataBase = database;
 			controller.campaignEngine = engine;
 			controller.CampaignKey = campaignkey;
-			
 			return controller;
 		}
 		
