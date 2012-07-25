996a0c91a69ec79f91998072ae88a2d99cca5ba5
Hagen Eckert <hagen.eckert@hotmail.com>
2012-07-25 14:59:09 +0200

    GCMLClientWeb in eigene Datei

---
+++ CampaignMasterWeb/GCMLClientWeb.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GenericCampaignMasterLib;
+
+namespace CampaignMasterWeb
+{
+    public struct GcmlClientKeys
+    {
+        public const string CAMPAIGNSTATE = "campaignstate";
+        public const string CAMPAIGNID = "campaignid";
+        public const string CONTEXTPLAYERID = "contextplayerid";
+        public const string CAMPAIGNCONTROLLER = "campaigncontroller";
+        public const string CONTEXTUNITID = "contextunitid";
+        public const string CONTEXTCOMMANDLIST = "contextcommandlist";
+        public const string SEKTORSTACK = "sektorstack";
+    }
+
+    public class GcmlClientWeb
+    {
+        public static void setCurrentPlayer(string id, HttpSessionState state)
+        {
+            Player player = GcmlClientWeb.getCampaignController(state).getPlayer(id);
+            state[GcmlClientKeys.CONTEXTPLAYERID] = id;
+        }
+
+        public static Player getCurrentPlayer(HttpSessionState state)
+        {
+            string id = (string)state[GcmlClientKeys.CONTEXTPLAYERID];
+            Player player = GcmlClientWeb.getCampaignController(state).getPlayer(id);
+            return player;
+        }
+
+        public static CampaignController getCampaignController(HttpSessionState state)
+        {
+            CampaignController controller;
+            CampaignBuilderTicTacTod cbttt = new CampaignBuilderTicTacTod();
+            string statekey = (string)state[GcmlClientKeys.CAMPAIGNSTATE];
+            string campaignkey = (string)state[GcmlClientKeys.CAMPAIGNID];
+
+            if (String.IsNullOrEmpty(statekey) || String.IsNullOrEmpty(campaignkey))
+            {
+                controller = cbttt.buildNew();      // Keine State vorhanden - neu erzeugen
+            }
+            else if ((CampaignController)state[GcmlClientKeys.CAMPAIGNCONTROLLER] != null)
+            {
+                controller = (CampaignController)state[GcmlClientKeys.CAMPAIGNCONTROLLER];
+            }
+            else
+            {
+                controller = cbttt.restoreFromDb(campaignkey, statekey);
+            }
+
+            // Ersten State speichern
+            string newkey = controller.saveCurrentGameState();
+            state[GcmlClientKeys.CAMPAIGNSTATE] = newkey;
+            state[GcmlClientKeys.CAMPAIGNID] = controller.CampaignKey;
+            state[GcmlClientKeys.CAMPAIGNCONTROLLER] = controller;
+
+#if DEBUG
+            //controller.restoreGameState(newkey);
+
+# endif
+
+            return controller;
+        }
+
+        public static Field getField(HttpSessionState state)
+        {
+            CampaignController controller = GcmlClientWeb.getCampaignController(state);
+            Field field = controller.campaignEngine.FieldField;
+            return field;
+        }
+
+        public static IUnit getUnitById(string unitId, HttpSessionState state)
+        {
+            CampaignController controller = GcmlClientWeb.getCampaignController(state);
+            IUnit unit = controller.getUnit(unitId);
+            return unit;
+        }
+    }
+}
--- CampaignMasterWeb/test.aspx.cs
+++ CampaignMasterWeb/test.aspx.cs
@@ -9,16 +9,6 @@ using GenericCampaignMasterLib;
 
 namespace CampaignMasterWeb
 {
-    public struct GcmlClientKeys
-    {
-        public const string CAMPAIGNSTATE = "campaignstate";
-        public const string CAMPAIGNID = "campaignid";
-        public const string CONTEXTPLAYERID = "contextplayerid";
-        public const string CAMPAIGNCONTROLLER = "campaigncontroller";
-        public const string CONTEXTUNITID = "contextunitid";
-        public const string CONTEXTCOMMANDLIST = "contextcommandlist";
-        public const string SEKTORSTACK = "sektorstack";
-    }
 
     public partial class GcmlClientPage : System.Web.UI.Page
     {
@@ -33,22 +23,6 @@ namespace CampaignMasterWeb
         {
 
         }
-
-
-        protected void setCurrentPlayer()
-        {
-            string id = dropDownPlayer.SelectedValue;
-            Player player = GcmlClientWeb.getCampaignController(this.Session).getPlayer(id);
-            Session[GcmlClientKeys.CONTEXTPLAYERID] = id;
-        }
-
-        protected Player getCurrentPlayer()
-        {
-            string id = (string)Session[GcmlClientKeys.CONTEXTPLAYERID];
-            Player player = GcmlClientWeb.getCampaignController(this.Session).getPlayer(id);
-            return player;
-        }
-
 
         protected void Button1_Click(object sender, EventArgs e)
         {
@@ -68,62 +42,11 @@ namespace CampaignMasterWeb
 
         protected void btnSelectPlayer_Click(object sender, EventArgs e)
         {
-            setCurrentPlayer();
+            string id = dropDownPlayer.SelectedValue;
+            GcmlClientWeb.setCurrentPlayer(id, this.Session);
             FieldControl1.drawPlayerContext();
         }
     }
 
-    public class GcmlClientWeb
-    {
-        public static CampaignController getCampaignController(HttpSessionState state)
-        {
-            CampaignController controller;
-            CampaignBuilderTicTacTod cbttt = new CampaignBuilderTicTacTod();
-            string statekey = (string)state[GcmlClientKeys.CAMPAIGNSTATE];
-            string campaignkey = (string)state[GcmlClientKeys.CAMPAIGNID];
-
-            if (String.IsNullOrEmpty(statekey) || String.IsNullOrEmpty(campaignkey))
-            {
-                controller = cbttt.buildNew();      // Keine State vorhanden - neu erzeugen
-            }
-            else if ((CampaignController)state[GcmlClientKeys.CAMPAIGNCONTROLLER] != null)
-            {
-                controller = (CampaignController)state[GcmlClientKeys.CAMPAIGNCONTROLLER];
-            }
-            else
-            {
-                controller = cbttt.restoreFromDb(campaignkey, statekey);
-            }
-
-            // Ersten State speichern
-            string newkey = controller.saveCurrentGameState();
-            state[GcmlClientKeys.CAMPAIGNSTATE] = newkey;
-            state[GcmlClientKeys.CAMPAIGNID] = controller.CampaignKey;
-            state[GcmlClientKeys.CAMPAIGNCONTROLLER] = controller;
-
-            #if DEBUG
-            //controller.restoreGameState(newkey);
-
-            # endif
-
-            return controller;
-        }
-
-        public static Field getField(HttpSessionState state)
-        {
-            CampaignController controller = GcmlClientWeb.getCampaignController(state);
-            Field field = controller.campaignEngine.FieldField;
-            return field;
-        }
-
-        public static IUnit getUnitById(string unitId, HttpSessionState state)
-        {
-            CampaignController controller = GcmlClientWeb.getCampaignController(state);
-            IUnit unit = controller.getUnit(unitId);
-            return unit;
-        }
-			
-
-
-    }
+   
 }
