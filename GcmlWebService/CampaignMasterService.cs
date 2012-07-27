3eed27743c8d972308a5716ef85ef3a6d57a639a
Hagen Eckert <hagen.eckert@hotmail.com>
2012-07-27 15:58:26 +0200

    Projekt für Webservice angelegt

---
+++ GcmlWebService/CampaignMasterService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GcmlWebService
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "CampaignMasterService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    public class CampaignMasterService : ICampaignMasterService
+    {
+        #region ICampaignMasterService Member
+
+        public string getPlayer(string playername)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> getPlayerCampaigns(string playerid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getFieldKoord(string campaignid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getSektor(string campaignid, string sektorkoord)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> getSektorList(string campaignid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getUnit(string campaignid, string unitid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> getUnitCollisions(string campaignid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> getCommandsForUnit(string campaignid, string unitid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string createNewCampaign(string playerid, string fielddimension)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addPlayerToCampaign(string playerid, string campaignid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void executeCommand(string campaignid, string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void addUnitToField(string campaignid, string unit, string targetsektor)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ GcmlWebService/ICampaignMasterService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GcmlWebService
+{
+    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Schnittstellennamen "ICampaignMasterService" sowohl im Code als auch in der Konfigurationsdatei ändern.
+    [ServiceContract]
+    public interface ICampaignMasterService
+    {
+        [OperationContract]
+        string getPlayer(string playername);
+
+        [OperationContract]
+        List<string> getPlayerCampaigns(string playerid);
+
+        [OperationContract]
+        string getFieldKoord(string campaignid);
+
+        [OperationContract]
+        string getSektor(string campaignid, string sektorkoord);
+
+        [OperationContract]
+        List<string> getSektorList(string campaignid);
+
+        [OperationContract]
+        string getUnit(string campaignid, string unitid);
+
+        [OperationContract]
+        List<string> getUnitCollisions(string campaignid);
+
+        [OperationContract]
+        List<string> getCommandsForUnit(string campaignid, string unitid);
+
+        [OperationContract]
+        string createNewCampaign(string playerid, string fielddimension);
+
+        [OperationContract]
+        void addPlayerToCampaign(string playerid, string campaignid);
+
+        [OperationContract]
+        void executeCommand(string campaignid, string command);
+
+        [OperationContract]
+        void addUnitToField(string campaignid, string unit, string targetsektor);
+    }
+}
