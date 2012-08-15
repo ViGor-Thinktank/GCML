ea28fea8a41dd0c2d24377415417d21fdbbd9465
Hagen Eckert <hagen.eckert@hotmail.com>
2012-08-15 14:43:47 +0200

    Ordner für Infoklassen
    Enthält reine Datenobjekte zur Kommunikation mit (Web)services

---
+++ GenericCampaignMasterLib/Code/Info/CommandInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCampaignMasterLib
+{
+    public struct CommandInfo
+    {
+        public string commandId;
+        public string commanType;
+        public string actingUnitId;
+    }
+}
