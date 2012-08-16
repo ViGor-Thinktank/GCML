a48e962ce4b2804f9ad5da0d586466eb52aa09df
Hagen Eckert <hagen.eckert@hotmail.com>
2012-08-16 11:00:00 +0200

    Singleton GcmlDataManager für Datenzugriffe durch WS

--- GcmlWebService/CampaignMasterService.svc.cs
+++ GcmlWebService/CampaignMasterService.svc.cs
@@ -10,22 +10,37 @@ using System.Web.Script.Serialization;
 
 namespace GcmlWebService
 {
-    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "Service1" sowohl im Code als auch in der SVC- und der Konfigurationsdatei ändern.
-    public class CampaignMasterService : ICampaignMasterService
+    public sealed class GcmlDataManager
     {
-        private JavaScriptSerializer m_serializer = new JavaScriptSerializer();
         private Dictionary<string, Player> m_playerDic = new Dictionary<string, Player>();
         private Dictionary<string, ICampaignDatabase> m_dictRunningCampaigns = new Dictionary<string, ICampaignDatabase>();
-        private string strStorepath = Environment.CurrentDirectory;
+        private string strStorepath = "d:\\temp\\";     // jaja ich weiss
 
-        #region ICampaignMasterService Member
+        private static readonly GcmlDataManager instance = new GcmlDataManager();
+
+        private GcmlDataManager() { }
+        public static GcmlDataManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public CampaignController getController(string campaignId)
+        {
+            ICampaignDatabase db = getCampaign(campaignId);
+            CampaignState state = db.getLastGameState();
+            CampaignEngine engine = state.Restore();
+            CampaignController controller = new CampaignController(engine);
+            controller.CampaignKey = campaignId;
+            return controller;
+        }
 
         public string getPlayerId(string playername)
         {
             string playerId = "";
-
-            var players = from cmp in m_dictRunningCampaigns.Values
-                          from p in cmp.getPlayerList()
+            var players = from p in m_playerDic.Values
                           where p.Playername == playername
                           select p;
 
@@ -45,30 +60,97 @@ namespace GcmlWebService
             return playerId;
         }
 
+        public Player getPlayer(string playerId)
+        {
+            if(m_playerDic.ContainsKey(playerId))
+                return m_playerDic[playerId];
+            else
+                return null;
+        }
+
+        public Dictionary<string, Player> getPlayerList()
+        {
+            return new Dictionary<string, Player>();
+        }
+
+        public ICampaignDatabase getCampaign(string id)
+        {
+            return null;
+        }
+        
+        public Dictionary<string, ICampaignDatabase> getRunningCampaigns()
+        {
+            return m_dictRunningCampaigns;
+        }
+
+        public string createNewCampaign(string playerid, string fielddimension)
+        {
+            Player player = getPlayer(playerid);
+            if (player == null)
+                return "";
+
+            // Datenbank
+            CampaignDatabaseRaptorDb database = new CampaignDatabaseRaptorDb();
+            database.CampaignKey = Guid.NewGuid().ToString();
+            database.StorePath = strStorepath;
+            database.init();
+
+            // Spielfeld
+            Field field = new Field(5, 5);
+
+            // Engine
+            CampaignEngine engine = new CampaignEngine(field);
+            engine.FieldField.Id = 123;
+            engine.addPlayer(player);
+            engine.addUnit(player, new DummyUnit(new Random().Next(1000, 9999).ToString()), field.getSektorList()[0]);
+
+            CampaignController controller = new CampaignController();
+            controller.CampaignDataBase = database;
+            controller.campaignEngine = engine;
+            controller.CampaignKey = database.CampaignKey;
+
+            database.saveGameState(engine.getState());
+            m_dictRunningCampaigns.Add(database.CampaignKey, database);
+
+            return database.CampaignKey;
+        }
+
+    }
+
+    public class CampaignMasterService : ICampaignMasterService
+    {
+        private JavaScriptSerializer m_serializer = new JavaScriptSerializer();
+       
+        #region ICampaignMasterService Member
+
+        public string getPlayerId(string playername)
+        {
+            return GcmlDataManager.Instance.getPlayerId(playername);   
+        }
+
         public List<string> getPlayerCampaigns(string playerid)
         {
-            var campaigns = from cmp in m_dictRunningCampaigns.Values
-                            from p in cmp.getPlayerList()
-                            where p.Id == playerid
-                            select cmp.CampaignKey;
-            return campaigns.ToList<string>();
+            Dictionary<string, ICampaignDatabase> campaigns = GcmlDataManager.Instance.getRunningCampaigns();
+
+            var plCampaigns =   from cmp in campaigns.Values
+                                from p in cmp.getPlayerList()
+                                where p.Id == playerid
+                                select cmp.CampaignKey;
+            return plCampaigns.ToList<string>();
         }
 
         public clsSektorKoordinaten getFieldKoord(string campaignid)
         {
-            CampaignController controller = getController(campaignid);
+            CampaignController controller = GcmlDataManager.Instance.getController(campaignid);
             clsSektorKoordinaten fieldKoord = controller.campaignEngine.FieldField.FieldDimension;
-
             return fieldKoord;
         }
 
         public Sektor getSektor(string campaignid, string sektorkoord)
         { 
-            //string resultSektor = "";
             clsSektorKoordinaten koord = (clsSektorKoordinaten)m_serializer.Deserialize<clsSektorKoordinaten>(sektorkoord);
-            CampaignController controller = getController(campaignid);
+            CampaignController controller = GcmlDataManager.Instance.getController(campaignid);
             Sektor sektor = controller.campaignEngine.FieldField.get(koord);
-
             return sektor;
         }
 
@@ -85,7 +167,7 @@ namespace GcmlWebService
         public List<Sektor> getUnitCollisions(string campaignid)
         {
             List<string> lstStrSektorClollisions = new List<string>();
-            CampaignController controller = getController(campaignid);
+            CampaignController controller = GcmlDataManager.Instance.getController(campaignid);
             return controller.getUnitCollisions();
         }
 
@@ -96,35 +178,7 @@ namespace GcmlWebService
 
         public string createNewCampaign(string playerid, string fielddimension)
         {
-            Player player = getPlayer(playerid);
-            if (player == null)
-                return "";
-
-            // Datenbank
-            CampaignDatabaseRaptorDb database = new CampaignDatabaseRaptorDb();
-            database.CampaignKey = Guid.NewGuid().ToString();
-            database.StorePath = strStorepath;
-            database.init();
-
-            // Spielfeld
-            Field field = new Field( 5, 5 );
-            
-            // Engine
-            CampaignEngine engine = new CampaignEngine(field);
-            engine.FieldField.Id = 123;
-            engine.addPlayer(player);
-            engine.addUnit(player, new DummyUnit(new Random().Next(1000, 9999).ToString()), field.getSektorList()[0]);
-
-            CampaignController controller = new CampaignController();
-            controller.CampaignDataBase = database;
-            controller.campaignEngine = engine;
-            controller.CampaignKey = database.CampaignKey;
-
-            database.saveGameState(engine.getState());
-            m_dictRunningCampaigns.Add(database.CampaignKey, database);
-            
-
-            return database.CampaignKey;
+            return GcmlDataManager.Instance.createNewCampaign(playerid, fielddimension);
         }
 
         public void addPlayerToCampaign(string playerid, string campaignid)
@@ -143,55 +197,6 @@ namespace GcmlWebService
         }
 
         #endregion
-
-
-        
-
-        private CampaignController getController(string campaignId)
-        {
-            ICampaignDatabase db = getCampaign(campaignId);
-            CampaignState state = db.getLastGameState();
-            CampaignEngine engine = state.Restore();
-            CampaignController controller = new CampaignController(engine);
-            controller.CampaignKey = campaignId;
-            return controller;
-        }
-
-        private Player getPlayer(string playerId)
-        {
-            Player player = null;
-            var players = from cmp in m_dictRunningCampaigns.Values
-                          from p in cmp.getPlayerList()
-                          where p.Id == playerId
-                          select p;
-
-            if (players.Count() > 0)
-                player = players.First();
-            else
-                player = m_playerDic[playerId];
-
-            return player;
-        }
-
-
-        private Dictionary<string, Player> getPlayerList()
-        {
-            return new Dictionary<string, Player>();
-        }
-
-        private ICampaignDatabase getCampaign(string id)
-        {
-            return null; 
-
-        }
-        private Dictionary<string, ICampaignDatabase> getRunningCampaigns()
-        {
-            return new Dictionary<string, ICampaignDatabase>();
-
-        }
-
-
-
     }
 
 }
