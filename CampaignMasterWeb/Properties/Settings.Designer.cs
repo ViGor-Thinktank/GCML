31e162d50853c81c956562c2b8b572af823fda2f
Hagen Eckert <hagen.eckert@hotmail.com>
2012-08-22 12:05:21 +0200

    GcmlDataManager in die Lib verschoben
    Stellt Zugriff auf verwaltete Spielerliste und Kampagnen bereit.

--- CampaignMasterWeb/Properties/Settings.Designer.cs
+++ CampaignMasterWeb/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace CampaignMasterWeb.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:666/GcmlWs/CampaignMasterService.svc")]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:8081/GcmlWebService/CampaignMasterService.svc")]
         public string CampaignMasterWeb_GcmlWs_CampaignMasterService {
             get {
                 return ((string)(this["CampaignMasterWeb_GcmlWs_CampaignMasterService"]));
