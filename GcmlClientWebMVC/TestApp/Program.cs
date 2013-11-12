fd9f7f6129783e00709f8e29c8f8a168defdb97a
Hagen Eckert <hagen.eckert@hotmail.com>
2013-11-12 23:10:13 +0100

    GcmlDataDb

--- GcmlClientWebMVC/TestApp/Program.cs
+++ GcmlClientWebMVC/TestApp/Program.cs
@@ -15,10 +15,21 @@ namespace TestApp
             CampaignController ctrl = CampaignBuilder.Instance.buildNew();
             CampaignState state = ctrl.CampaignEngine.getState();
 
-            GcmlDbContext ctx = new GcmlDbContext();
-            ctx.CampaignStates.Add(state);
-            
+            //using (GcmlDbContext ctx = new GcmlDbContext())
+            //{
+            //    //ctx.CampaignStates.Add(state);
+            //    //
 
+            //    //ctx.
+            //    //ctx.SaveChanges();
+
+            //}
+
+            GcmlDataDb db = new GcmlDataDb();
+            db.safeCampaignState(state);
+            state.ListPlayers.Add(new PlayerInfo() { playerId = "1234", playerName = "asdf" });
+
+            db.safeCampaignState(state);
 
         }
     }
--- GcmlDataAccess/SQLServer/GcmlDataDb.cs
+++ GcmlDataAccess/SQLServer/GcmlDataDb.cs
@@ -7,6 +7,8 @@ using GenericCampaignMasterModel;
 
 namespace GcmlDataAccess
 {
+    
+    // Todo: Umbenennen - ist die eigentliche DataAccess-Klasse.
     public class GcmlDataDb
     {
         public CampaignState getCampaignState(string campaignId)
@@ -14,18 +16,22 @@ namespace GcmlDataAccess
             return CampaignState.NewInstance();
         }
 
-        //public CampaignState newCampaign(CampaignInfo info)
-        //{
-        //    return CampaignState.NewInstance();
-        //}
-
         public bool safeCampaignState(CampaignState state)
         {
             bool result = false;
-            using (var gcmlcontext = new GcmlDbContext())
+            using (var ctx = new GcmlDbContext())
             {
-                gcmlcontext.CampaignStates.Add(state);
-                gcmlcontext.SaveChanges();
+                var existing = ctx.CampaignStates.Find(state.CampaignId);
+                if (existing == null)
+                {
+                    ctx.CampaignStates.Add(state);
+                }
+                else
+                {
+                    existing = state;
+                }
+
+                ctx.SaveChanges();
                 result = true;
             }
             return result;
@@ -34,23 +40,62 @@ namespace GcmlDataAccess
         public List<PlayerInfo> getPlayers()
         {
             List<PlayerInfo> result =  new List<PlayerInfo>();
+            using (var ctx = new GcmlDbContext())
+            {
+                result = ctx.Players.ToList<PlayerInfo>();
+            }
 
             return result;
         }
 
         public PlayerInfo getPlayerInfo(string playerId)
         {
-            return new PlayerInfo();
+            PlayerInfo result = new PlayerInfo();
+            using (var ctx = new GcmlDbContext())
+            {
+                result = ctx.Players.Where(p => p.playerId == playerId).FirstOrDefault();
+            }
+
+            return result;
         }
 
         public List<CampaignInfo> getCampaignsForPlayer(string playerId)
         {
-            return new List<CampaignInfo>();
+            List<CampaignInfo> result =  new List<CampaignInfo>();
+            using(var ctx = new GcmlDbContext())
+            {
+                PlayerInfo player = ctx.Players.Where(p => p.playerId == playerId).FirstOrDefault();
+                if (player != null)
+                {
+                    var states = from state in ctx.CampaignStates
+                                 where state.ListPlayers.Contains(player)
+                                 select new CampaignInfo()
+                                           {
+                                               campaignId = state.CampaignId,
+                                               campaignName = state.CampaignName,
+                                               players = state.ListPlayers
+                                           };
+                    result = states.ToList<CampaignInfo>();
+                }
+            }
+            return result;
         }
 
         public bool safePlayer(PlayerInfo info)
         {
-            return true;
+            bool result = false;
+            using (var ctx = new GcmlDbContext())
+            {
+                var existing = ctx.Players.Find(info);
+                if (existing == null)
+                    ctx.Players.Add(info);
+                else
+                    existing.playerName = info.playerName;
+                ctx.SaveChanges();
+                result = true;
+            }
+
+            return result;
         }
     }
 }
